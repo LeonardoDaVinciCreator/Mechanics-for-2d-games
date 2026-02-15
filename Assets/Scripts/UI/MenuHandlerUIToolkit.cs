c60f6ee69fad551e0b1f77263587f372615ece4e
DiCaprioLeonardo <149061169+LeonardoDaVinciCreator@users.noreply.github.com>
2026-02-15 23:13:42 +0500

    base ui with ui toolkit

--- Assets/Scripts/UI/MenuHandlerUIToolkit.cs
+++ Assets/Scripts/UI/MenuHandlerUIToolkit.cs
@@ -1,43 +1,110 @@
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UIElements;
+
+
 
 public class MenuHandlerUIToolkit : MonoBehaviour
 {
     private UIDocument _uiDoc;
     private VisualElement _root;
-    private VisualElement _panel;
+    private VisualElement _pauseMenu;
+    private VisualElement _settingsMenu;
+    private VisualElement _mainMenu;
 
+    private Action _pauseAction;
+    private Action _settingsAction;
 
+    private Button _pauseMenuButton;
+    private Button _shopMenuButton;    
+
+    private Button _exitSettingButton;
+
+    [SerializeField] private bool _isPaused;
+    [SerializeField] private bool _isSettings;
 
     private void OnEnable()
     {
         _uiDoc = GetComponent<UIDocument>();
         _root = _uiDoc.rootVisualElement;
+        _pauseAction = ToglePauseMenu;
+        _settingsAction = TogleSettingsMenu;
 
         //поиск элемента по классу внутри VisualElement
-        _panel = _root.Q<VisualElement>("panel");//Q- первый элемент в последовательности
-        if(_panel == null) return;
+        _pauseMenu = _root.Q<VisualElement>("PauseMenu");//Q- первый элемент в последовательности
+        _settingsMenu = _root.Q<VisualElement>("SettingsMenu");
+        _shopMenuButton = _root.Q<Button>("ShopButton");
+       
+        _pauseMenuButton = _root.Q<Button>("PauseButton");
+        if(_pauseMenuButton == null || _pauseMenu == null) return;
+        _pauseMenuButton.clicked += _pauseAction;
 
-        List<Button> buttons = _panel.Query<Button>(className: "menu-button").ToList();//Query-все элементы в последовательности
+        var pauseLabel = _pauseMenu.Q<Label>("PauseLabel");
+        var pauseMenuCard = _pauseMenu.Q<VisualElement>("PauseMenuCard");
+        List<Button> buttons = pauseMenuCard.Query<Button>(className: "menu-button").ToList();//Query-все элементы в последовательности     
 
-        Label panelLabel = _panel.Q<Label>(className:"label");
-        if(panelLabel != null)
+        Label settingsLabel = _settingsMenu.Q<Label>("SettingsLabel");
+        if(_settingsMenu != null)
         {
-            panelLabel.text = "Нажми на кнопку";
+            var settingsMenuCard = _settingsMenu.Q<VisualElement>("SettingsMenuCard");
+            if(settingsMenuCard != null)
+            {                
+                var applyBtn = settingsMenuCard.Q<Button>("ApplyButton");
+                var cancelBtn = settingsMenuCard.Q<Button>("CancelButton");
+                
+                applyBtn.clicked += _settingsAction;
+                cancelBtn.clicked += _settingsAction;
+            }
         }
+        
+
 
         foreach(var btn in buttons)
         {
-            btn.clicked += () =>
+            switch (btn.name)
             {
-                Debug.Log($"Нажата кнопка: {btn.name}");
-                UpdateLabel($"Нажата кнопка: {btn.name}", panelLabel);
-            };
+                case "ResumeButton":
+                {
+                    btn.clicked += _pauseAction;
+                    break;
+                }
+                case "SettingsButton":
+                {
+                    btn.clicked += _settingsAction;
+                    break;
+                }
+                case "MainMenuButton":
+                {
+                    btn.clicked += TogleMainMenu;
+                    break;
+                }
+                
+            }            
         }
 
         
+    }
+
+    private void TogleMainMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TogleSettingsMenu()
+    {
+        if(_settingsMenu == null) return;        
+        _isSettings = !_isSettings;
+        if (_isSettings)
+        {
+            _pauseMenu.RemoveFromClassList("show");
+            _settingsMenu.AddToClassList("show");            
+        }
+        else
+        {
+            _settingsMenu.RemoveFromClassList("show");
+            _pauseMenu.AddToClassList("show");
+        }
     }
 
     private void UpdateLabel(string newText, Label label)
@@ -45,14 +112,48 @@ public class MenuHandlerUIToolkit : MonoBehaviour
         label.text = newText;
     }
 
-    private void Osable()
+    private void ToglePauseMenu()
     {
-        if(_panel == null) return;
+        if(_pauseMenu == null) return;
+        _isPaused = !_isPaused;
 
-        var buttons = _panel.Query<Button>(className:"menu-button").ToList();
-        foreach(var btn in buttons)
+        if (_isPaused)
         {
-            btn.clicked -= null;
+            Time.timeScale = 0;
+            _pauseMenu.AddToClassList("show");//добавление класса show в css
+            _pauseMenuButton.SetEnabled(false);
+            _pauseMenuButton.AddToClassList("hide");
+            _shopMenuButton.AddToClassList("hide");
+            _shopMenuButton.SetEnabled(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            _pauseMenu.RemoveFromClassList("show");///удаление класса show в css
+            _pauseMenuButton.SetEnabled(true);
+            _pauseMenuButton.RemoveFromClassList("hide");
+            _shopMenuButton.RemoveFromClassList("hide");
+            _shopMenuButton.SetEnabled(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(_pauseMenuButton == null) return;
+        _pauseMenuButton.clicked -= _pauseAction;
+        if(_pauseMenu == null || _settingsMenu == null) return;        
+
+        CleanupMenuButtons();
+    }
+
+    private void CleanupMenuButtons()
+    {
+        var allMenus = new[] { _pauseMenu, _settingsMenu };
+        foreach(var menu in allMenus)
+        {
+            if(menu == null) continue;
+            var buttons = menu.Query<Button>(className: "menu-button").ToList();
+            foreach(var btn in buttons) btn.clicked -= null;
         }
     }
 }
