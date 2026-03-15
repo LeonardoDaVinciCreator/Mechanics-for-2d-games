8bf008aa6a6f041b494a48c80377a5cf1460564b
DiCaprioLeonardo <149061169+LeonardoDaVinciCreator@users.noreply.github.com>
2026-03-15 20:25:29 +0500

    Fixed Climber connection logic from WallDisappearing

--- Assets/Scripts/Bullet/Climber.cs
+++ Assets/Scripts/Bullet/Climber.cs
@@ -34,9 +34,18 @@ public class Climber : MonoBehaviour, ILaunchable
         if (!collision.gameObject.CompareTag("Wall")) return;
          
         WallBase wall = collision.gameObject.GetComponent<WallBase>();
+        if(wall != null && _attachedWall == null)
+        {
+            _attachedWall = wall;
+
+            _attachedWall.OnWallActived += WallActived;
+            _attachedWall.OnWallDeactived += WallDeactived;
+
+            _attachedWall.ActivateWall();
+            
+            AttachWall(collision.gameObject);
+        }
         
-        wall?.ActivateWall();
-        AttachWall(collision.gameObject);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -45,25 +54,71 @@ public class Climber : MonoBehaviour, ILaunchable
         
         // Деактивация при отрыве
         WallBase wall = collision.gameObject.GetComponent<WallBase>();
-        wall?.DeactivateWall(); // Возврат в исходное положение
+        if(wall != null && wall == _attachedWall)
+        {
+            
+            DetachWall();
+        }
+        
     }
 
     private void AttachWall(GameObject wall)
     {
+        _isAttached = true;
+
         _rb.linearVelocity = Vector2.zero;
         _rb.angularVelocity = 0;
         _rb.bodyType = RigidbodyType2D.Kinematic;
-
-        _attachedWall = wall.GetComponent<WallBase>();
 
         transform.parent = wall.transform;
 
         gameObject.tag = "ActiveBird";
     }
 
-    public void Reset()
+    private void DetachWall()
     {
         _rb.bodyType = RigidbodyType2D.Dynamic;
         transform.parent = null;
+
+        _attachedWall.OnWallActived -= WallActived;
+        _attachedWall.OnWallDeactived -= WallDeactived;
+
+        _attachedWall.DeactivateWall(); // Возврат в исходное положение
+        _attachedWall = null;
+
+        _isAttached = false;
+        _rb.bodyType = RigidbodyType2D.Dynamic;
+        transform.parent = null;
+        gameObject.tag = "Untagged";
+    }    
+
+    private void WallActived()
+    {
+        Debug.Log("активация звука и тд, запуск можно сделать через события");
+    }
+
+    private void WallDeactived()
+    {
+        Debug.Log("активация звука и может быть тряска камеры");
+    }
+
+    public void Reset()
+    {
+        if (_attachedWall != null)
+        {
+            _attachedWall.OnWallActived -= WallActived;
+            _attachedWall.OnWallDeactived -= WallDeactived;
+            _attachedWall = null;
+        }
+
+        _isAttached = false;
+        _rb.bodyType = RigidbodyType2D.Dynamic;
+        transform.parent = null;
+        gameObject.tag = "Untagged";
+    }
+
+    private void OnDestroy()
+    {
+       Reset();
     }
 }
